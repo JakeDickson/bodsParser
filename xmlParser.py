cce4b245a060169441d193686ec493b2771c3c2a
JakeDickson <jakefdickson@gmail.com>
2022-08-21 21:58:27 +0100

    Iterate through files in folder and output row

--- xmlParser.py
+++ xmlParser.py
@@ -23,38 +23,42 @@ class tableRow:
         self.destination = 'Destination'
 
 #move file search import after static defines
-file_name = 'xml/bods3.xml'
-full_file = os.path.abspath(os.path.join('', file_name))
-tree = ElementTree.parse(full_file)
-root = tree.getroot()
+cwd = os.getcwd() + '/xml/'
+fileList = os.listdir(cwd)
+fileList[:] = [x for x in fileList if x.endswith('.xml')]
 
-insertRow = tableRow()
+#Assuming there is files in directory etc...
+for filePath in fileList:
+    tree = ElementTree.parse(cwd + filePath)
+    root = tree.getroot()
 
-for operatorID in root.findall(operatorPath):
-    print(operatorID.attrib['id']) #get operator ID
-    setattr(insertRow, 'operatorID', operatorID.attrib['id'])
+    insertRow = tableRow()
 
-for noc in root.findall(nationalOperatorCodePath):
-    if noc.tag == (http + "NationalOperatorCode"):
-        print(noc.text) #NationalOperatorCode
-        setattr(insertRow, 'noc', noc.text)
+    for operatorID in root.findall(operatorPath):
+        print(operatorID.attrib['id']) #get operator ID
+        setattr(insertRow, 'operatorID', operatorID.attrib['id'])
 
-for line in root.findall(linePath):
-    if line.tag == http + "LineName":
-        print(line.text) #LineName
-        setattr(insertRow, 'lineName', line.text)
+    for noc in root.findall(nationalOperatorCodePath):
+        if noc.tag == (http + "NationalOperatorCode"):
+            print(noc.text) #NationalOperatorCode
+            setattr(insertRow, 'noc', noc.text)
 
-for service in root.findall(servicePath):
-    if service.tag == http + "ServiceCode":
-        print(service.tag, service.text) #Service Code/ID?
-        setattr(insertRow, 'serviceCode', service.text)
+    for line in root.findall(linePath):
+        if line.tag == http + "LineName":
+            print(line.text) #LineName
+            setattr(insertRow, 'lineName', line.text)
 
-for serviceRoute in root.findall(standardServicePath):
-    if serviceRoute.tag == http + "Origin":
-        print(serviceRoute.tag, serviceRoute.text) #Origin
-        setattr(insertRow, 'origin', serviceRoute.text)
-    if serviceRoute.tag == http + "Destination":
-        print(serviceRoute.tag, serviceRoute.text) #Destination
-        setattr(insertRow, 'destination', serviceRoute.text)
+    for service in root.findall(servicePath):
+        if service.tag == http + "ServiceCode":
+            print(service.tag, service.text) #Service Code/ID?
+            setattr(insertRow, 'serviceCode', service.text)
 
-print(insertRow.serviceCode, insertRow.operatorID, insertRow.lineName, insertRow.origin, insertRow.destination)
+    for serviceRoute in root.findall(standardServicePath):
+        if serviceRoute.tag == http + "Origin":
+            print(serviceRoute.tag, serviceRoute.text) #Origin
+            setattr(insertRow, 'origin', serviceRoute.text)
+        if serviceRoute.tag == http + "Destination":
+            print(serviceRoute.tag, serviceRoute.text) #Destination
+            setattr(insertRow, 'destination', serviceRoute.text)
+
+    print(insertRow.serviceCode, insertRow.operatorID, insertRow.lineName, insertRow.origin, insertRow.destination)
